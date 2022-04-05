c74f09081e270ca3cb6dac41b50830bb68c5ded6
tafaramafemba <tafara.maf@gmail.com>
2022-04-05 18:18:11 +0200

    fix linter errors

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
-import "./style.css";
+import './style.css';
 const scoreboard = document.querySelector('.scoreboard');
 
 const data = [
   {
-    name: ['Tafara', 'Tom', 'Brady' ],
+    name: ['Tafara', 'Tom', 'Brady'],
     score: [100, 200, 400],
   },
 ];
 
-for(let i = 0; i < 3; i += 1) {
-scoreboard.innerHTML = `
+for (let i = 0; i < 3; i += 1) {
+  scoreboard.innerHTML = `
 
 <div class = "board">
 <p class = "d-name">${data[0].name[0]}</p>
@@ -17,5 +17,4 @@ scoreboard.innerHTML = `
 
 </div>
 `;
-};
-
+}
