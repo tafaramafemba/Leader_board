634f7730e1c9780adcac2ef16cd69a43bf7b9f2d
tafaramafemba <tafara.maf@gmail.com>
2022-04-05 18:08:43 +0200

    dummy text for leaderboard

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import "./style.css";
+const scoreboard = document.querySelector('.scoreboard');
+
+const data = [
+  {
+    name: ['Tafara', 'Tom', 'Brady' ],
+    score: [100, 200, 400],
+  },
+];
+
+for(let i = 0; i < 3; i += 1) {
+scoreboard.innerHTML = `
+
+<div class = "board">
+<p class = "d-name">${data[0].name[0]}</p>
+<p class = "d-name">${data[0].score[0]}</p>
+
+</div>
+`;
+};
+
